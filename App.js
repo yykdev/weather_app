c1aece2c95b70bb64d56109e36b25f7196a45688
yykdev <assavictory@gmail.com>
2019-10-04 19:38:50 +0900

    openweathermap api 연동

--- App.js
+++ App.js
@@ -2,10 +2,19 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import Axios from "axios";
+
+const API_KEY = "65c91756dff7105ae51e2b48e7e16563";
 
 export default class extends React.Component {
     state = {
         isLoading: true
+    };
+    getWeather = async (latitude, longitude) => {
+        const { data } = await Axios.get(
+            `http://api.openweathermap.org/data/2.5/weather?APPID=${API_KEY}&lat=${latitude}&lon=${longitude}&units=metric`
+        );
+        console.log(data);
     };
     getLocation = async () => {
         try {
@@ -13,6 +22,7 @@ export default class extends React.Component {
             const {
                 coords: { latitude, longitude }
             } = await Location.getCurrentPositionAsync();
+            this.getWeather(latitude, longitude);
             this.setState({ isLoading: false });
         } catch (error) {
             Alert.alert("Can't find you.", "So sad");
