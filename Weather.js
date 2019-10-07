99b2fb0fba5ca3ef008d00bd03e139124d721d75
yykdev <assavictory@gmail.com>
2019-10-07 15:29:37 +0900

    날씨 상태별 배경 그라데이션, 날씨 아이콘 변경 셋팅 - 케이스 별 초이스 객체 화

--- Weather.js
+++ Weather.js
@@ -1,19 +1,63 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 
-export default function Weather({ temp }) {
+const weatherOptions = {
+    Thunderstorm: {
+        iconName: "weather-lightning",
+        gradient: ["#373B44", "#4286f4"]
+    },
+    Drizzle: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"]
+    },
+    Rain: {
+        iconName: "weather-rainy",
+        gradient: ["#00C6FB", "#005BEA"]
+    },
+    Snow: {
+        iconName: "weather-snowy",
+        gradient: ["#7DE2FC", "#B9B6E5"]
+    },
+    Atmosphere: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"]
+    },
+    Clear: {
+        iconName: "weather-sunny",
+        gradient: ["#FF7300", "#FEF253"]
+    },
+    Clouds: {
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC", "#304352"]
+    },
+    Mist: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+    },
+    Dust: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+    },
+    Haze: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+    }
+};
+
+export default function Weather({ temp, condition }) {
     return (
         <LinearGradient
-            colors={["#4c669f", "#3b5998", "#192f6a"]}
+            colors={weatherOptions[condition].gradient}
             style={styles.container}
         >
+            <StatusBar barStyle="light-content" />
             <View style={styles.halfContainer}>
                 <MaterialCommunityIcons
                     size={96}
-                    name="weather-lightning-rainy"
+                    name={weatherOptions[condition].iconName}
                     color="white"
                 />
                 <Text style={styles.temp}>{temp}°</Text>
